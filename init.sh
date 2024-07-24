1d20c5b5ba996b7de0d49ed7200bca3f780a843a
amir taherkhani <amirtaherkhani@outlook.com>
2024-07-24 15:30:54 +0330

    first commit

---
+++ init.sh
@@ -0,0 +1,12 @@
+# Try to get project root from current directory
+PROJECT_ROOT=$(dirname "$(pwd)")
+
+mkdir -p ${PROJECT_ROOT}/src/certs
+mkdir -p ${PROJECT_ROOT}/src/data/logs
+mkdir -p ${PROJECT_ROOT}/src/volumes
+
+# Create the files (replace with your desired filenames)
+touch "${PROJECT_ROOT}/src/certs/acme.json"
+touch "${PROJECT_ROOT}/src/configs/traefik.yml"
+
+echo "Files created successfully in ${PROJECT_ROOT}/src"
