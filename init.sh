fa647aec4c00a68eb67a8436c3179f19af694bf0
Amir <amirtaherkhani@outlook.com>
2024-08-02 09:09:35 +0000

    Update configs for docker-mailserver

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 # Try to get project root from current directory
-PROJECT_ROOT=$(dirname "$(pwd)")
+PROJECT_ROOT=$(pwd)
 
 mkdir -p ${PROJECT_ROOT}/src/certs
 mkdir -p ${PROJECT_ROOT}/src/data/logs
 mkdir -p ${PROJECT_ROOT}/src/volumes
+
+chmod 600 ${PROJECT_ROOT}/src/certs/acme.json
 
 # Create the files (replace with your desired filenames)
 touch "${PROJECT_ROOT}/src/certs/acme.json"
